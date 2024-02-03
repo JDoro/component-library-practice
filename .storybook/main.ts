fbc9d11e1235b5576e70ae51109cc643d84ebd71
jdoro <jdoro@nvisia.com>
2024-02-03 10:52:27 -0600

    Small lint fixes and adding code coverage

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,4 @@
-import type {StorybookConfig} from "@storybook/react-vite";
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -9,8 +9,9 @@ const config: StorybookConfig = {
     "@storybook/addon-interactions",
     "@storybook/addon-storyshots",
     "@storybook/addon-testrunner",
-    '@storybook/addon-viewport',
-    '@storybook/addon-a11y'
+    "@storybook/addon-viewport",
+    "@storybook/addon-a11y",
+    "@storybook/addon-coverage",
   ],
   framework: {
     name: "@storybook/react-vite",
