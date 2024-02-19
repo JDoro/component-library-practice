9b495e1f0631c8e4b89aceaff1a63fefb9af6f08
Joshua Doro <doroj@alumni.msoe.edu>
2024-02-18 19:06:41 -0600

    Added Portal Component

---
+++ src/atoms/portal/Portal.tsx
@@ -0,0 +1,16 @@
+import { ReactNode, useEffect } from "react";
+import { createPortal } from "react-dom";
+
+export function Portal({ children }: { children: ReactNode }) {
+  const appBody = document.body;
+  const element = document.createElement("div");
+
+  useEffect(() => {
+    appBody.appendChild(element);
+    return () => {
+      appBody.removeChild(element);
+    };
+  }, [appBody, element]);
+
+  return createPortal(children, element);
+}
