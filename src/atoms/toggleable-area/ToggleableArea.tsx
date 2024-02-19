2d9dd8872958f71871853934730493f29d765c9b
Joshua Doro <doroj@alumni.msoe.edu>
2024-02-18 19:24:13 -0600

    Added ToggleableArea component.

---
+++ src/atoms/toggleable-area/ToggleableArea.tsx
@@ -0,0 +1,13 @@
+import { ReactNode, useState } from "react";
+
+interface ToggleableAreaProps {
+  toggle: (showArea: () => void) => ReactNode;
+  toggledArea: (hideArea: () => void) => ReactNode;
+}
+export function ToggleableArea({toggle, toggledArea}: ToggleableAreaProps) {
+  const [show, setShow] = useState(false);
+  return <>
+    {toggle(() => setShow(true))}
+    {show && toggledArea(() => setShow(false))}
+  </>
+}
