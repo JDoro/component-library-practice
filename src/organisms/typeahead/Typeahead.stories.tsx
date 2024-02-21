87a2b6d2f570e748fb4f0d88540c8c89a71d206d
Joshua Doro <doroj@alumni.msoe.edu>
2024-02-20 21:04:45 -0600

    Started adding a very simple typeahead

---
+++ src/organisms/typeahead/Typeahead.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Typeahead } from "./Typeahead.tsx";
+import { useArgs } from "@storybook/preview-api";
+
+const meta = {
+  title: "Example/Typeahead",
+  component: Typeahead,
+  tags: ["autodocs"],
+  render: (args) => {
+    const [{ value, onChange, onSelect }, updateArgs] = useArgs();
+    return (
+      <div className="w-64">
+        <Typeahead
+          {...args}
+          value={value}
+          onChange={(value) => {
+            onChange?.();
+            updateArgs({ value });
+          }}
+          onSelect={(value) => {
+            onSelect?.();
+            updateArgs({ value });
+          }}
+        />
+      </div>
+    );
+  },
+} satisfies Meta<typeof Typeahead>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const All: Story = {
+  args: {
+    options: ["one", "two", "three"],
+  }
+};
